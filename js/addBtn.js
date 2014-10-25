c6a7dcf31721d4e6a348234a00faa18fcd883e69
pokerG <pokerfacehlg@gmail.com>
2014-10-25 22:58:26 +0800

    add newemail btn but must enlarge and lessness

--- js/addBtn.js
+++ js/addBtn.js
@@ -1,30 +1,36 @@
-var state = false;
-function insert_btn() {
-	node = document.querySelector(".wG");
-	node = node.parentNode;
-	if (node != null && !state) {
-		node.style.right = "200px";
-		console.log(node);
-		var element = document.createElement("div");
+function insert_btn(parent) {
+	toolbar = parent.querySelector(".pXSFsb"); //邮件的附件,图片工具栏
+	if (toolbar == null) {
+		return;
+	}
+
+	node = toolbar.children[0];
+	if (node.childElementCount == 5) { //判断是否已添加Btn
+		var element = document.createElement("button");
 		element.setAttribute("name", "generate_btn");
 		element.setAttribute("id", "test_btn");
+		element.setAttribute("class", "btn")
 		element.innerText = "test";
 		node.appendChild(element);
 		state = true;
-	}else{
-		console.log("!!");
+		element.onclick = "test()";
+		return element;
 	}
-
-
 }
-document.onmousemove = function(){
-	text = document.querySelector('.Am,.aO9,.Al,.editable,.LW-avf');
-	if(text != null){
-		if(document.activeElement.className == text.className){
-			insert_btn();
+document.onmousemove = function() {
+	reply = document.querySelector('.Am,.aO9,.Al,.editable,.LW-avf'); //回复
+	newemail = document.querySelectorAll('.AD');	//新邮件
+	if (reply != null) {
+		if (document.activeElement.className == reply.className) {
+			insert_btn(document);
 		}
 	}
+	for (var i = 0; i < newemail.length; i++) {
+		insert_btn(newemail[i]);
+	}
 }
 
 
-
+function test() {
+	console.log("hello");
+}
