2c88d1ed92a90c2abc9f7f1cbda98941afafcae4
ChanceLi <1993lchsh@163.com>
2014-12-12 01:15:37 +0800

    sort without showing

--- js/addModal.js
+++ js/addModal.js
@@ -10,6 +10,8 @@ var pageAttNum = 10;
 var pageNum = 0;
 var searchPageNum = 0;
 var keyFlag = 1;
+var sort = new Array();
+var sortFlag = true;
 
 Array.prototype.indexOf = function(val) {
 	for (var i = 0; i < this.length; i++) {
@@ -40,11 +42,81 @@ function scrollBottom() {
 
 function changeSize(size) {
 	if (size < Math.pow(1024, 2))
-		return ((size / 1024).toFixed(2) + "KB");
+		return ((size / 1024).toFixed(2) + " KB");
 	else if (size < Math.pow(1024, 3))
-		return ((size / Math.pow(1024, 2)).toFixed(2) + "MB");
+		return ((size / Math.pow(1024, 2)).toFixed(2) + " MB");
 	else
-		return ((size / Math.pow(1024, 3)).toFixed(2) + "GB");
+		return ((size / Math.pow(1024, 3)).toFixed(2) + " GB");
+}
+
+function changeDate(date) {
+	var dat0= date.split(', ');
+	var dat1 = dat0[1].split(' ');
+	var month = dat1[1];
+	var day = dat0[0];
+	switch(dat1[1]) {
+		case 'Jan':
+			month = '01';
+			break;
+		case 'Feb':
+			month = '02';
+			break;
+		case 'Mar':
+			month = '03';
+			break;
+		case 'Apr':
+			month = '04';
+			break;
+		case 'May':
+			month = '05';
+			break;
+		case 'Jun':
+			month = '06';
+			break;
+		case 'Jul':
+			month = '07';
+			break;
+		case 'Aug':
+			month = '08';
+			break;
+		case 'Sep':
+			month = '09';
+			break;
+		case 'Oct':
+			month = '10';
+			break;
+		case 'Nov':
+			month = '11';
+			break;
+		case 'Dec':
+			month = '12';
+			break;
+	}
+	switch(dat0[0]) {
+		case 'Mon':
+			day = '星期一';
+			break;
+		case 'Tue':
+			day = '星期二';
+			break;
+		case 'Wed':
+			day = '星期三';
+			break;
+		case 'Thu':
+			day = '星期四';
+			break;
+		case 'Fri':
+			day = '星期五';
+			break;
+		case 'Sat':
+			day = '星期六';
+			break;
+		case 'Sun':
+			day = '星期日';
+			break;
+	}
+	var newDate = dat1[2] + '年' + month + '月' + dat1[0] + '日' + dat1[3] + ' ' + day;
+	return(newDate);
 }
 
 function addAtt(att) {
@@ -69,7 +141,7 @@ function addAtt(att) {
 	c3.innerHTML = att.subject;
 	c4.innerHTML = att.from;
 	c5.innerHTML = att.to;
-	c6.innerHTML = dat[0];
+	c6.innerHTML = changeDate(dat[0]);
 	return (row);
 }
 
@@ -95,7 +167,8 @@ function addSel(att) {
 	c3.innerHTML = att.subject;
 	c4.innerHTML = att.from;
 	c5.innerHTML = att.to;
-	c6.innerHTML = dat[0];
+	c6.innerHTML = changeDate(dat[0]);
+	return (row);
 }
 
 //分页
@@ -167,13 +240,13 @@ function addSeclet() {
 	var ch = document.getElementsByName('attCheck');
 	for (i = 0; i < ch.length; i++) {
 		var tr = ch[i].parentNode.parentNode;
-		var index = tr.rowIndex;
+		var index = parseInt(tr.id.split('tt')[1]);
 		if (ch[i].checked == true) {
-			if (selIndex.contain(index - 1) == -1) {
-				addSel(attach[index - 1]);
-				selected.push(attach[i]);
-				selIndex.push(i);
-				//alert(selIndex);
+			if (selIndex.contain(index) == -1) {
+				var row = addSel(attach[index]);
+				row.id = "sel" + index;
+				selected.push(attach[index]);
+				selIndex.push(index);
 			}
 			flag++;
 			ch[i].checked = false;
@@ -201,6 +274,8 @@ function deleteSelect() {
 			selected.splice(i, 1);
 			selIndex.splice(i, 1);
 			selectTable.deleteRow(index);
+			//alert(index);
+			//alert(tr.id);
 			flag++;
 		}
 		//alert(selected);
@@ -307,8 +382,8 @@ function submitModal() {
 	loading.setAttribute("class", "KA Kj-JD picker-dialog");
 	loading.setAttribute("id", "loadingModal");
 	loading.setAttribute("role", "dialog");
-	loading.setAttribute("style", "margin:0px auto; width:180px; height:22px;overflow:auto; left:450px; top:0px; background: #fff1a8; border-color: #f9edbe; overflow:auto; font-size:20px;");
-	loading.innerHTML="创建邮件...";
+	loading.setAttribute("style", "margin:0px auto; width:180px; height:22px;overflow:auto; left:45%; top:0px; background: #fff1a8; border-color: #f9edbe; overflow:auto; font-size:20px;");
+	loading.innerHTML="创建邮件中...";
 	body.appendChild(loading);
 	chrome.extension.sendMessage({
 		cmd: "draft",
@@ -347,6 +422,106 @@ function disKey() {
 		keyFlag = 0;
 }
 
+//排序显示（未完成）
+function reshow() {
+	var searchTable = document.getElementById('attTable');
+	var sortCell = new Array();
+	var i;
+	for (i = 0; i < sort.length; i++) {
+		var sortNode = document.getElementById('att' + sort[i].index);
+		sortCell.push(sortNode);
+	}
+	if (sortFlag)
+		for (i = 0; i < sort.length; i++)
+			searchTable.rows[i + 1] = sortCell[i];
+	else
+		for (i = 0; i < sort.length; i++)
+			searchTable.rows[sort.length - i + 1] = sortCell[i];
+	//page();
+	//alert(sortCell.length + "!");
+	//未完成
+	sortFlag = !sortFlag;
+	//alert(sortFlag);
+}
+
+function bubbleSort() {
+	var i,j;
+	var temp,string;
+	for (i = 0; i < sort.length - 1; i++)
+		for (j = i + 1; j < sort.length; j++) 
+			if (sort[i].key > sort[j].key) {
+				//alert(i + ' ' + j);
+				temp = sort[i];
+				sort[i] = sort[j];
+				sort[j] = temp;
+			}
+	reshow();
+	string = '';
+	for (i = 0; i < sort.length; i++)
+		string += sort[i].index + ' ' + sort[i].key + '\n';
+	alert(string);
+}
+
+function doSort(sortKey) {
+	var i;
+	var searchTable = document.getElementById('attTable');
+	sort.splice(0, sort.length);
+	for (i = 1; i < searchTable.rows.length; i++) {
+		var row = searchTable.rows[i];
+		var sortNode = {
+			index : parseInt(row.id.split('tt')[1]),
+			key : row.cells[sortKey].innerHTML
+		}
+		if (sortKey == 7)
+			sortNode.key = attach[sortNode.index].size;
+		else if (sortKey == 0) {
+			sortNode.key = row.cells[sortKey].children[0].checked;
+		}
+		sort.push(sortNode);
+	}
+	bubbleSort();
+}
+
+function choseSort() {
+	alert("chose");
+	doSort(0);
+}
+
+function typeSort() {
+	alert("type");
+	doSort(1);
+}
+
+function nameSort() {
+	alert("name");
+	doSort(2);
+}
+
+function subjectSort() {
+	alert("subject");
+	doSort(3);
+}
+
+function fromSort() {
+	alert("from");
+	doSort(4);
+}
+
+function toSort() {
+	alert("to");
+	doSort(5);
+}
+
+function dateSort() {
+	alert("date");
+	doSort(6);
+}
+
+function sizeSort() {
+	alert("size");
+	doSort(7);
+}
+
 function addModal(parent) {
 	//alert('add'); 
 	body = parent.querySelector(".aAU");
@@ -356,7 +531,7 @@ function addModal(parent) {
 		var divBack = document.createElement("div");
 		divBack.setAttribute("id", "divBack");
 		divBack.setAttribute("class", "KA Kj-JD-Jh");
-		divBack.setAttribute("style", "opacity: 0.6; width: 1366px; height: 800px; background-color:gray;");
+		divBack.setAttribute("style", "opacity: 0.6; width: 100%; height: 100%; background-color:gray;");
 		divBack.setAttribute("aria-hidden", "true");
 		body.appendChild(divBack);
 
@@ -364,7 +539,7 @@ function addModal(parent) {
 		divIframe.setAttribute("class", "KA Kj-JD picker-dialog");
 		divIframe.setAttribute("id", "divmodal");
 		divIframe.setAttribute("role", "dialog");
-		divIframe.setAttribute("style", "margin:0px auto; width:1120px; overflow:auto; left:100px; top:50px; border:2px solid gray; overflow:auto");
+		divIframe.setAttribute("style", "margin:0px auto; width:85%; overflow:auto; left:7%; top:8%; border:2px solid gray; overflow:auto");
 		divIframe.border = "0px";
 
 		var clsbut = document.createElement("img");
@@ -400,19 +575,19 @@ function addModal(parent) {
 
 		//alert("ok");
 
-		var install = "<div id='iframemodal'><br/><br/><div class='row' style='margin:0px auto; width:1100px; height:500px;'>";
+		var install = "<div id='iframemodal'><br/><br/><div class='row' style='margin:0px auto; width:90%; height:500px;'>";
 		install += "<div class='modal-footer' style='background-color:white;'>";
 		install += "<div><div class='span10'><form class='form-search'>";
 		install += "<div class='input-append'><input id='keyword' type='text' value='要搜索的附件...' class='span10'>";
 		install += "<button id = 'search'  class='btn btn-primary btn-middle btn-danger'>搜索</button></form></div></div>";
 		install += "<div class='container-fluid' id='exArea'><div id='ex'><table class='table table-hover table-bordered'pa_ui_name='table,exinput' pa_ui_hover='true'pa_ui_selectable='true' pa_ui_select_mode='multi'pa_ui_select_trigger='tr' pa_ui_select_column='0'pa_ui_select_triggerelement=':checkbox' id='attTable'><caption><h3 id='attTitle'>附件列表</h3></caption>";
-		install += "<thead><tr><th style='width:36px;'>选择</th><th style='width:72px;'>附件类型</th><th>附件名</th><th style='width:144px;'>主题</th><th>发件人</th><th>收件人</th><th>日期</th><th style='width:72px;'>附件大小</th></tr></thead><tbody></tbody></table>";
+		install += "<thead><tr><th style='width:40px;' id='_chose'><a hidefocus='' style='hide-focus: true'target='_blank'>选择</a></th><th style='width:72px;' id='_type'><a hidefocus='' style='hide-focus: true'target='_blank'>类型</a></th><th id='_name'><a hidefocus='' style='hide-focus: true'target='_blank'>附件名</a></th><th style='width:144px;' id='_subject'><a hidefocus='' style='hide-focus: true'target='_blank'>主题</a></th><th id='_from'><a hidefocus='' style='hide-focus: true'target='_blank'>发件人</a></th><th id='_to'><a hidefocus='' style='hide-focus: true'target='_blank'>收件人</a></th><th style='width:144px;' id='_date'><a hidefocus='' style='hide-focus: true'target='_blank'>日期</a></th><th style='width:72px;' id='_size'><a hidefocus='' style='hide-focus: true'target='_blank'>大小</a></th></tr></thead><tbody></tbody></table>";
 		install += "<div class='pagination'style='margin: auto; width: 480px; text-align: center;'><ul id='changePage'><li><a href='#' style='color: white; background-color: #ee5f5b;' id='prevPage'>";
 		install += "Prev</a></li><li><a href='#' style='color: blue;' id='nowPage'>1</a></li><li><a href='#' style='color: white; background-color: #ee5f5b;' id='nextPage'>Next</a></li></ul><ul id='searchPage' style='display:none;'><li><a href='#' style='color: white; background-color: #ee5f5b;' id='searchPrevPage'>";
 		install += "Prev</a></li><li><a href='#' style='color: blue;' id='searchNowPage'>1</a></li><li><a href='#' style='color: white; background-color: #ee5f5b;' id='searchNextPage'>Next</a></li></ul></div><div class='btn-group'style='margin: auto; text-align: right;'>";
 		install += "<button class='btn btn-primary btn-middle btn-danger' id='addSeclet'>添加选中附件</button></div></div></div></div>";
 		install += "<div class='modal-footer'><div><div><table class='table table-hover table-bordered'pa_ui_name='table,exinput' pa_ui_hover='true'pa_ui_selectable='true' pa_ui_select_mode='multi'pa_ui_select_trigger='tr' pa_ui_select_column='0'pa_ui_select_triggerelement=':checkbox' id='selectTable'><caption><h3 id='selTitle'>选中附件列表(0)</h3>";
-		install += "</caption><thead><tr><th style='width:36px;'>选择</th><th style='width:72px;'>附件类型</th><th>附件名</th><th style='width:144px;'>主题</th><th>发件人</th><th>收件人</th><th>日期</th><th style='width:72px;'>附件大小</th></tr></thead><tbody></tbody></table>";
+		install += "</caption><thead><tr><th style='width:40px;'>选择</th><th style='width:72px;'>类型</th><th>附件名</th><th style='width:144px;'>主题</th><th>发件人</th><th>收件人</th><th style='width:144px;'>日期</th><th style='width:72px;'>大小</th></tr></thead><tbody></tbody></table>";
 		install += "<div class='btn-group'style='margin: auto; text-align: right;'><button class='btn btn-primary btn-middle btn-danger' id='deleteSelect'>删除选中附件</button><button class='btn btn-primary btn-middle btn-danger' id='submitModal'>创建草稿</button><button class='btn btn-primary btn-middle btn-danger' id='download'>下载选中附件</button></div></div></div></div></div></div>";
 
 		//alert(install);
@@ -428,6 +603,15 @@ function addModal(parent) {
 		document.getElementById("download").addEventListener("click", download, false);
 		document.getElementById("keyword").addEventListener("blur", disKey, false);
 		document.getElementById("keyword").addEventListener("focus", showKey, false);
+		document.getElementById("_chose").addEventListener("click", choseSort, false);
+		document.getElementById("_type").addEventListener("click", typeSort, false);
+		document.getElementById("_name").addEventListener("click", nameSort, false);
+		document.getElementById("_subject").addEventListener("click", subjectSort, false);
+		document.getElementById("_from").addEventListener("click", fromSort, false);
+		document.getElementById("_to").addEventListener("click", toSort, false);
+		document.getElementById("_date").addEventListener("click", dateSort, false);
+		document.getElementById("_size").addEventListener("click", sizeSort, false);
+		
 
 	}
 }
